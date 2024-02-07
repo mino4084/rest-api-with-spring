8849915791197d2a43657d84e2cc965b9f529119
mino <mino4084@gmail.com>
2024-02-07 17:52:16 +0900

    Event 도메인 생성

---
+++ src/test/java/com/example/restapiwithspring/events/EventTest.java
@@ -0,0 +1,33 @@
+package com.example.restapiwithspring.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("rest api event")
+                .description("REST API development")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
